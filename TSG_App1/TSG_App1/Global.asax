af0c0e4dbbb4800423b43423d9d74279638da650
ramirocds1 <rrodriguez@codigodelsur.com>
2015-05-25 15:39:18 -0300

    commit inicial

---
+++ TSG_App1/TSG_App1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TSG_App1.MvcApplication" Language="C#" %>
---
+++ TSG_App1/TSG_App1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TSG_App1.Startup))]
+namespace TSG_App1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
